548c64c420a67db64d49c3cfda1d06c93783dece
Carlos <carloosfrs95@gmail.com>
2024-02-22 21:59:27 -0300

    Busqueda arreglo

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,9 +3,7 @@ from AppCoder import views
 
 urlpatterns = [
     path('', views.inicio, name="Inicio"), #esta era nuestra primer view
-    path('pedido', views.pedido, name="Pedido"),
     path('cliente', views.Cliente, name="Cliente"),
-    path('vendedor', views.Vendedores, name="Vendedor"),
     path('carrito', views.Carrito, name="Carrito"),
     path('pedidoFormulario', views.pedidoFormulario, name="PedidoFormulario"),
     path('vendedorFormulario', views.vendedorFormulario, name="VendedorFormulario"),
